e74cc13a085e48c0276b18a2391a34cfc92231a8
Chrudos Vorlicek <chrudos.vorlicek@gmail.com>
2020-06-28 20:46:16 +0200

    Add .gitignore, composer, config, phpstan, package and route files

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/o-nas', function () {
+    return view('about');
 });
+
+Route::get('/akce', function () {return view('event/events');})->name('akce');
+Route::get('/detail-akce/{event?}', 'EventController@get')->name('event-detail');
+Route::post('/events', 'EventController@index')->name('events');
